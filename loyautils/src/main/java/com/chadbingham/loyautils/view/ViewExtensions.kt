9f5e7574cb3a4182c58c9f48f1adbf6651e3fac4
Chad Bingham <Chad.Bingham52@gmail.com>
2017-09-03 18:44:57 -0600

    Added View Extensions

---
+++ loyautils/src/main/java/com/chadbingham/loyautils/view/ViewExtensions.kt
@@ -0,0 +1,27 @@
+package com.chadbingham.loyautils.view
+
+import android.view.View
+
+fun View.clicks() : Observable{
+
+}
+
+fun View.onClick(listener: View.OnClickListener) {
+    setOnClickListener(listener)
+}
+
+fun View.onClick(onClick: (v: View) -> Unit) {
+    setOnClickListener(onClick)
+}
+
+fun View.invisible() {
+    visibility = View.INVISIBLE
+}
+
+fun View.visible() {
+    visibility = View.VISIBLE
+}
+
+fun View.gone() {
+    visibility = View.GONE
+}
