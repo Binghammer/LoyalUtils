c4d6572bd48c45443d6adbf74fbef30c4b367195
Chad Bingham <Chad.Bingham52@gmail.com>
2017-09-07 20:28:51 -0600

    added extension

--- loyautils/src/main/java/com/chadbingham/loyautils/misc/PropertyDelegate.kt
+++ loyautils/src/main/java/com/chadbingham/loyautils/misc/PropertyDelegate.kt
@@ -3,7 +3,15 @@ package com.chadbingham.loyautils.misc
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
+fun <T> T.delegate(): PropertyDelegate<Any, T> {
+    return PropertyDelegate(this)
+}
+
 class PropertyDelegate<R, T>(private var t: T) : ReadWriteProperty<R, T> {
+
+    operator fun provideDelegate(thisRef: R, property: KProperty<*>) {
+
+    }
 
     override fun getValue(thisRef: R, property: KProperty<*>): T {
         return t
