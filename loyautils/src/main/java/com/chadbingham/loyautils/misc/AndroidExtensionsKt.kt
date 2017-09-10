1ab05e79caafef107199acbfe977d14ecfae2533
Chad Bingham <Chad.Bingham52@gmail.com>
2017-09-10 10:51:44 -0600

    Added contect extensions

--- loyautils/src/main/java/com/chadbingham/loyautils/misc/AndroidExtensionsKt.kt
+++ loyautils/src/main/java/com/chadbingham/loyautils/misc/AndroidExtensionsKt.kt
@@ -1,6 +1,12 @@
 package com.chadbingham.loyautils.misc
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
 import android.os.Build
+import android.support.v4.content.ContextCompat
+import android.support.v4.graphics.drawable.DrawableCompat
 
 inline fun apiOr(version: Int, action_greater: () -> Unit, action_lower: () -> Unit, inclusive: Boolean = false) {
     fromApi(version, action_greater, inclusive)
@@ -13,4 +19,23 @@ inline fun toApi(toVersion: Int, action: () -> Unit, inclusive: Boolean = false)
 
 inline fun fromApi(fromVersion: Int, action: () -> Unit, inclusive: Boolean = true) {
     if (Build.VERSION.SDK_INT > fromVersion || (inclusive && Build.VERSION.SDK_INT == fromVersion)) action()
+}
+
+fun Context.drawable(drawableId: Int): Drawable {
+    return ContextCompat.getDrawable(this, drawableId)
+}
+
+fun Context.bitmap(drawableId: Int): Bitmap {
+    var drawable = ContextCompat.getDrawable(this, drawableId)
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+        drawable = DrawableCompat.wrap(drawable).mutate()
+    }
+
+    val bitmap = Bitmap.createBitmap(drawable.intrinsicWidth,
+            drawable.intrinsicHeight, Bitmap.Config.ARGB_8888)
+    val canvas = Canvas(bitmap)
+    drawable.setBounds(0, 0, canvas.width, canvas.height)
+    drawable.draw(canvas)
+
+    return bitmap
 }
