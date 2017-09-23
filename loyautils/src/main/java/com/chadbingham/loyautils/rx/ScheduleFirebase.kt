68d53b7e3386f5913bcec1b6a8ca6e93235c0d0b
Chad Bingham <Chad.Bingham52@gmail.com>
2017-09-23 12:50:04 -0600

    More ext for RX

---
+++ loyautils/src/main/java/com/chadbingham/loyautils/rx/ScheduleFirebase.kt
@@ -0,0 +1,31 @@
+package com.chadbingham.loyautils.rx
+
+import io.reactivex.*
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.Executors
+
+object ScheduleFirebase {
+
+    private val scheduler = Schedulers.from(Executors.newSingleThreadExecutor())
+
+    fun <T> flowable(): FlowableTransformer<T, T> {
+        return FlowableTransformer { o -> o.subscribeOn(scheduler).observeOn(AndroidSchedulers.mainThread()) }
+    }
+
+    fun <T> observable(): ObservableTransformer<T, T> {
+        return ObservableTransformer { o -> o.subscribeOn(scheduler).observeOn(AndroidSchedulers.mainThread()) }
+    }
+
+    fun <T> single(): SingleTransformer<T, T> {
+        return SingleTransformer { o -> o.subscribeOn(scheduler).observeOn(AndroidSchedulers.mainThread()) }
+    }
+
+    fun <T> maybe(): MaybeTransformer<T, T> {
+        return MaybeTransformer { o -> o.subscribeOn(scheduler).observeOn(AndroidSchedulers.mainThread()) }
+    }
+
+    fun completable(): CompletableTransformer {
+        return CompletableTransformer { o -> o.subscribeOn(scheduler).observeOn(AndroidSchedulers.mainThread()) }
+    }
+}
