1944efbece7b6947eef259ade6c361cfa5855463
Chad Bingham <Chad.Bingham52@gmail.com>
2017-09-30 12:52:25 -0600

    removed renderscript

--- loyautils/src/main/java/com/chadbingham/loyautils/view/BlurUtil.kt
+++ loyautils/src/main/java/com/chadbingham/loyautils/view/BlurUtil.kt
@@ -1,11 +1,6 @@
 package com.chadbingham.loyautils.view
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.support.v8.renderscript.Allocation
-import android.support.v8.renderscript.Element
-import android.support.v8.renderscript.RenderScript
-import android.support.v8.renderscript.ScriptIntrinsicBlur
 
 /**
  * Be sure to add these to your defaultConfig in build.gradle:
@@ -17,16 +12,16 @@ import android.support.v8.renderscript.ScriptIntrinsicBlur
  */
 class BlurUtil(context: Context) {
 
-    private val renderScript = RenderScript.create(context)!!
-
-    fun blur(bitmap: Bitmap, radius: Float = 8F) {
-        //this will blur the bitmap with a radius of 8 and save it in bitmap
-        val input = Allocation.createFromBitmap(renderScript, bitmap)
-        val output = Allocation.createTyped(renderScript, input.type)
-        val script = ScriptIntrinsicBlur.create(renderScript, Element.U8_4(renderScript))
-        script.setRadius(radius)
-        script.setInput(input)
-        script.forEach(output)
-        output.copyTo(bitmap)
-    }
+//    private val renderScript = RenderScript.create(context)!!
+//
+//    fun blur(bitmap: Bitmap, radius: Float = 8F) {
+//        //this will blur the bitmap with a radius of 8 and save it in bitmap
+//        val input = Allocation.createFromBitmap(renderScript, bitmap)
+//        val output = Allocation.createTyped(renderScript, input.type)
+//        val script = ScriptIntrinsicBlur.create(renderScript, Element.U8_4(renderScript))
+//        script.setRadius(radius)
+//        script.setInput(input)
+//        script.forEach(output)
+//        output.copyTo(bitmap)
+//    }
 }
