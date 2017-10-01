7b8a5e9d9b2d536f97350a6bb2d197bc9e183b34
Chad Bingham <Chad.Bingham52@gmail.com>
2017-10-01 13:53:20 -0600

    Updates to rx.Event

--- loyautils/src/main/java/com/chadbingham/loyautils/rx/Event.kt
+++ loyautils/src/main/java/com/chadbingham/loyautils/rx/Event.kt
@@ -1,3 +1,5 @@
+@file:Suppress("unused")
+
 package com.chadbingham.loyautils.rx
 
 typealias EmptyEvent<T> = Event.Empty<T>
@@ -6,19 +8,24 @@ typealias AddedEvent<T> = Event.Added<T>
 typealias RemovedEvent<T> = Event.Removed<T>
 typealias ChangedEvent<T> = Event.Changed<T>
 typealias CancelledEvent<T> = Event.Canceled<T>
+typealias ErrorEvent<T> = Event.Error<T>
 
 sealed class Event<out T>(val value: T?) {
 
     val hasValue: Boolean
         get() = value != null
 
-    class Empty<out T> : Neutral<T>(null)
+    val isPositive: Boolean
+        get() = this is Positive
+
+    val isNegative: Boolean
+        get() = this is Negative
 
     open class Positive<out T>(value: T) : Event<T>(value)
 
     open class Negative<out T>(value: T?) : Event<T>(value)
 
-    open class Neutral<out T>(value: T?) : Event<T>(value)
+    class Empty<out T> : Event<T>(null)
 
     /* For events that were added before the observer subscribed. */
     class Original<out T>(value: T) : Positive<T>(value)
@@ -29,5 +36,7 @@ sealed class Event<out T>(val value: T?) {
 
     class Removed<out T>(value: T? = null) : Negative<T>(value)
 
-    class Canceled<out T>(val error: Throwable? = null) : Event<T>(null)
+    class Canceled<out T>(val message: String? = null) : Negative<T>(null)
+
+    class Error<out T>(val error: Throwable? = null) : Negative<T>(null)
 }
